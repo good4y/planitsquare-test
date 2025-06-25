50d16c6455da7ee3cbef8942712b901d08cb5c62
kim-youngchul <kmosh1212@naver.com>
2025-06-25 22:49:58 +0900

    chore: 스프링 기초 설정

---
+++ src/main/java/com/planitsquaretest/PlanitSquareTestApplication.java
@@ -0,0 +1,13 @@
+package com.planitsquaretest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanitSquareTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlanitSquareTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/planitsquaretest/PlanitSquareTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planitsquaretest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanitSquareTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
