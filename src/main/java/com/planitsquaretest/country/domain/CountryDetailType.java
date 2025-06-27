a26c7cbbc768cb896bd17e70bf19f56262e315e7
kim-youngchul <kmosh1212@naver.com>
2025-06-27 11:24:23 +0900

    feat: detail type 에 description 추가

--- src/main/java/com/planitsquaretest/country/domain/CountryDetailType.java
+++ src/main/java/com/planitsquaretest/country/domain/CountryDetailType.java
@@ -1,5 +1,14 @@
 package com.planitsquaretest.country.domain;
 
+import lombok.Getter;
+
+@Getter
 public enum CountryDetailType {
-    GOOGLE_CALENDAR_ID
+    GOOGLE_CALENDAR_ID("google holiday calendar id");
+
+    private final String description;
+
+    CountryDetailType(String s) {
+        this.description = s;
+    }
 }
