e1e57fdfbe144eb12f2e78fe84236b0318b04800
kim-youngchul <kmosh1212@naver.com>
2025-06-27 10:34:27 +0900

    refactor: 예외 클래스 throwable 파라메터 추가

--- src/main/java/com/planitsquaretest/common/exception/InternalServerErrorException.java
+++ src/main/java/com/planitsquaretest/common/exception/InternalServerErrorException.java
@@ -6,4 +6,16 @@ public class InternalServerErrorException extends BaseException{
     public InternalServerErrorException(String message) {
         super(message, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
+    public InternalServerErrorException(String message, Throwable cause) {
+        super(message, HttpStatus.INTERNAL_SERVER_ERROR, cause);
+    }
+
+    public static InternalServerErrorException serverError(String message) {
+        return new InternalServerErrorException(message);
+    }
+
+    public static InternalServerErrorException serverError(String message,  Throwable cause) {
+        return new InternalServerErrorException(message, cause);
+    }
 }
