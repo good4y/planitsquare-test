9df2138bbefa6e2e04f404aa6313aecf1d9c463e
kim-youngchul <kmosh1212@naver.com>
2025-06-27 16:37:13 +0900

    refactor: CsvUtil 실패 갯수 카운팅 기능 추가

--- src/main/java/com/planitsquaretest/common/utils/CsvUtil.java
+++ src/main/java/com/planitsquaretest/common/utils/CsvUtil.java
@@ -1,25 +1,39 @@
 package com.planitsquaretest.common.utils;
 
 import com.opencsv.bean.CsvToBeanBuilder;
-import com.planitsquaretest.common.exception.InternalServerErrorException;
+import com.opencsv.bean.CsvToBeanFilter;
+import com.planitsquaretest.common.dto.CsvResultResponse;
 
 import java.io.Reader;
+import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class CsvUtil {
 
     private CsvUtil() {
     }
 
-    public static <T> List<T> read(Reader reader, Class<T> dtoType) {
-        try {
-            return new CsvToBeanBuilder<T>(reader)
-                    .withType(dtoType)
-                    .withIgnoreLeadingWhiteSpace(true)
-                    .build()
-                    .parse();
-        } catch (Exception e) {
-            throw InternalServerErrorException.serverError("csv read error: " + e.getMessage());
-        }
+    public static <T> CsvResultResponse<T> read(Reader reader, Class<T> dtoType) {
+        AtomicInteger failed = new AtomicInteger(0);
+
+        CsvToBeanFilter filter = line -> {
+            boolean isNull = Arrays.stream(line).anyMatch(s -> s == null || s.isBlank() || s.equals("null"));
+            if (isNull) {
+                failed.incrementAndGet();
+                return false;
+            }
+
+            return true;
+        };
+
+        List<T> data = new CsvToBeanBuilder<T>(reader)
+                .withType(dtoType)
+                .withIgnoreLeadingWhiteSpace(true)
+                .withFilter(filter)
+                .build()
+                .parse();
+
+        return new CsvResultResponse<>(data, failed.get());
     }
 }
--- src/test/java/com/planitsquaretest/common/utils/CsvUtilTest.java
+++ src/test/java/com/planitsquaretest/common/utils/CsvUtilTest.java
@@ -1,5 +1,6 @@
 package com.planitsquaretest.common.utils;
 
+import com.planitsquaretest.common.dto.CsvResultResponse;
 import com.planitsquaretest.country.dto.GoogleCalendarCsvDto;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -30,16 +31,41 @@ class CsvUtilTest {
             Reader reader = new StringReader(csv);
 
             // when
-            List<GoogleCalendarCsvDto> googleCalendarCsvDtos = CsvUtil.read(reader, GoogleCalendarCsvDto.class);
+            CsvResultResponse<GoogleCalendarCsvDto> googleCalendarCsvDtos = CsvUtil.read(reader, GoogleCalendarCsvDto.class);
 
             // then
-            GoogleCalendarCsvDto result = googleCalendarCsvDtos.getFirst();
+            List<GoogleCalendarCsvDto> data = googleCalendarCsvDtos.data();
+            int failedCount = googleCalendarCsvDtos.failedCount();
 
-            assertThat(result).isNotNull();
-            assertThat(result.getCountryCode()).isEqualTo(koreaIsoCode);
-            assertThat(result.getCountry()).isEqualTo(countryName);
-            assertThat(result.getCalendarId()).isEqualTo(calendarId);
+            assertThat(failedCount).isEqualTo(0);
+            assertThat(data.size()).isEqualTo(1);
+            assertThat(data.getFirst().getCalendarId()).isEqualTo(calendarId);
+            assertThat(data.getFirst().getCountryCode()).isEqualTo(koreaIsoCode);
+            assertThat(data.getFirst().getCountry()).isEqualTo(countryName);
         }
 
+        @Test
+        void csv_데이터_중_빈_컬럼을_읽고_에러를_기록한다() {
+            // given
+            String koreaIsoCode = "KR";
+            String countryName = "South Korea";
+
+            String csv = String.format("""
+                    CountryCode,Country,CalendarId
+                    %s,%s,%s
+                    """, koreaIsoCode, countryName, null);
+
+            Reader reader = new StringReader(csv);
+
+            // when
+            CsvResultResponse<GoogleCalendarCsvDto> googleCalendarCsvDtos = CsvUtil.read(reader, GoogleCalendarCsvDto.class);
+
+            // then
+            List<GoogleCalendarCsvDto> data = googleCalendarCsvDtos.data();
+            int failedCount = googleCalendarCsvDtos.failedCount();
+
+            assertThat(failedCount).isEqualTo(1);
+            assertThat(data.size()).isEqualTo(0);
+        }
     }
 }
