79371746299137947f010379c47a7bd96c3f7966
kim-youngchul <kmosh1212@naver.com>
2025-06-28 17:33:23 +0900

    feat: 서버 시작 시 nager api를 이용하여 country 추가

---
+++ src/main/java/com/planitsquaretest/init/CountryInitRunner.java
@@ -0,0 +1,24 @@
+package com.planitsquaretest.init;
+
+import com.planitsquaretest.country.facade.CountryCommandFacade;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Order(200)
+@Component
+@RequiredArgsConstructor
+public class CountryInitRunner implements ApplicationRunner {
+
+    private final CountryCommandFacade countryCommandFacade;
+
+    @Override
+    public void run(ApplicationArguments args) {
+        log.info("CountryInitRunner started");
+        countryCommandFacade.saveAllCountryFromNager();
+    }
+}
