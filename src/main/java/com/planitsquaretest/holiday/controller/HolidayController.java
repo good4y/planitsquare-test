b9b4292b56ca505e1365c8237ad035b31d5dd92f
kim-youngchul <kmosh1212@naver.com>
2025-06-28 23:40:25 +0900

    feat: refresh, delete 기능 api 작성

--- src/main/java/com/planitsquaretest/holiday/controller/HolidayController.java
+++ src/main/java/com/planitsquaretest/holiday/controller/HolidayController.java
@@ -3,6 +3,7 @@ package com.planitsquaretest.holiday.controller;
 import com.planitsquaretest.api.HolidayApi;
 import com.planitsquaretest.holiday.dto.HolidaySearchConditionDto;
 import com.planitsquaretest.holiday.dto.HolidaySearchResponseDto;
+import com.planitsquaretest.holiday.facade.HolidayCommandFacade;
 import com.planitsquaretest.holiday.facade.HolidayQueryFacade;
 import com.planitsquaretest.model.HolidayViewResponse;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +16,17 @@ import java.util.List;
 public class HolidayController implements HolidayApi {
 
     private final HolidayQueryFacade holidayQueryFacade;
+    private final HolidayCommandFacade holidayCommandFacade;
+
+    @Override
+    public void holidayDelete(Integer year, String countryCode) {
+        holidayCommandFacade.deleteHoliday(year, countryCode);
+    }
+
+    @Override
+    public void holidayRefresh(Integer year, String countryCode) {
+        holidayCommandFacade.refreshHolidayFromNager(year, countryCode);
+    }
 
     @Override
     public List<HolidayViewResponse> holidaySearch(Integer year, String countryCode, Integer offset, Integer limit) {
